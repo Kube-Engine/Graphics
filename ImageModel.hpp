2436ab5063ad5a8d6c5cf2c001a3a7c4c2f9617f
pedro <pierre.veysseyre@epitech.eu>
2020-11-11 03:29:30 +0100

    Add some helper function to create ImageModels

--- ImageModel.hpp
+++ ImageModel.hpp
@@ -15,14 +15,14 @@ namespace kF::Graphics
 struct kF::Graphics::ImageModel : public VkImageCreateInfo
 {
     /** @brief Initialize constructor */
-    ImageModel(const ImageCreateFlags flags_, const ImageType imageType_, const Format format_, const Extent3D &extent_,
+    ImageModel(const ImageCreateFlags flags_, const ImageType type_, const Format format_, const Extent3D &extent_,
             const std::uint32_t mipLevels_, const std::uint32_t arrayLayers_, const SampleCountFlags samples_,
-            const ImageTiling tiling_, const Usage usage_, const SharingMode sharingMode_) noexcept
+            const ImageTiling tiling_, const ImageUsageFlags usage_, const SharingMode sharingMode_, const uint32_t queueFamilyIndexCount_, const uint32_t *pQueueFamilyIndices_) noexcept
         : VkImageCreateInfo(
             sType: VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
             pNext: nullptr,
             flags: createFlags_,
-            imageType: imageType_,
+            imageType: type_,
             format: format_,
             extent: extent_,
             mipLevels: mipLevels_,
@@ -41,4 +41,111 @@ struct kF::Graphics::ImageModel : public VkImageCreateInfo
     ImageModel(ImageModel &&other) noexcept = default;
     ImageModel &operator=(const ImageModel &other) noexcept = default;
     ImageModel &operator=(ImageModel &&other) noexcept = default;
+
+    [[nodiscard]] static ImageModel MakeBasicImage(const ImageType type, const Extent3D &extent, const Format format, const SampleCountFlags samples) noexcept {
+        return ImageModel(
+            0,
+            type,
+            format,
+            Extent3D {
+                width: extent.width,
+                height: (type != ImageType::Type1D ? extent.height : 1),
+                depth: (type == ImageType::Type3D ? extent.depth : 1)
+            },
+            1,
+            1,
+            samples,
+            ImageTiling::TilingOptimal,
+            ImageUsageFlags::TransferDstBit | ImageUsageFlags::SampledBit,
+            SharingMode::Exclusive,
+            0,
+            nullptr,
+            ImageLayout::Undefined
+        );
+    }
+
+    // [[nodiscard]] static ImageModel MakeBasic2D(const Extent3D &extent, const Format format, const SampleCountFlags samples) noexcept {
+    //     return ImageModel(
+    //         0,
+    //         ImageType::Type2D,
+    //         format,
+    //         Extent3D {
+    //             width: extent.width,
+    //             height: extent.height,
+    //             depth: 1
+    //         },
+    //         1,
+    //         1,
+    //         samples,
+    //         ImageTiling::TilingOptimal,
+    //         ImageUsageFlags::TransferBit | ImageUsageFlags::SampledBit,
+    //         SharingMode::Exclusive,
+    //         0,
+    //         nullptr,
+    //         ImageLayout::Undefined
+    //     );
+    // }
+
+    // [[nodiscard]] static ImageModel MakeBasic3D(const Extent3D &extent, const Format format, const SampleCountFlags samples) noexcept {
+    //     return ImageModel(
+    //         0,
+    //         ImageType::Type3D,
+    //         format,
+    //         extent,
+    //         1,
+    //         1,
+    //         samples,
+    //         ImageTiling::TilingOptimal,
+    //         ImageUsageFlags::TransferBit | ImageUsageFlags::SampledBit,
+    //         SharingMode::Exclusive,
+    //         0,
+    //         nullptr,
+    //         ImageLayout::Undefined
+    //     );
+    // }
+
+    [[nodiscard]] static ImageModel MakeLayerImage(const ImageType type, const Extent3D &extent, const Format format, const SampleCountFlags samples, const uint32_t layers) noexcept {
+        return ImageModel(
+            0,
+            ImageType::Type2D,
+            format,
+            Extent3D {
+                width: extent.width,
+                height: (type != ImageType::Type1D ? extent.height : 1),
+                depth: (type == ImageType::Type3D ? extent.depth : 1)
+            },
+            layers ? layers : 1,
+            1,
+            samples,
+            ImageTiling::TilingOptimal,
+            ImageUsageFlags::TransferBit | ImageUsageFlags::SampledBit,
+            SharingMode::Exclusive,
+            0,
+            nullptr,
+            ImageLayout::Undefined
+        );
+    }
+
+    [[nodiscard]] static ImageModel MakeMipMapImage(const ImageType type, const Extent3D &extent, const Format format, const SampleCountFlags samples, const uint32_t mipLevels) noexcept {
+        return ImageModel(
+            0,
+            ImageType::Type2D,
+            format,
+            Extent3D {
+                width: extent.width,
+                height: (type != ImageType::Type1D ? extent.height : 1),
+                depth: (type == ImageType::Type3D ? extent.depth : 1)
+            },
+            1,
+            mipLevels ? mipLevels : 1,
+            samples,
+            ImageTiling::TilingOptimal,
+            ImageUsageFlags::TransferBit | ImageUsageFlags::SampledBit,
+            SharingMode::Exclusive,
+            0,
+            nullptr,
+            ImageLayout::Undefined
+        );
+    }
+
 };
