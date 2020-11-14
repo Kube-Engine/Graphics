a188120f21f488c0a66331f5a626b600b00c497b
pedro <pierre.veysseyre@epitech.eu>
2020-11-14 21:20:21 +0100

    Update Descriptor set update handler with copy model

--- DescriptorSetUpdate.hpp
+++ DescriptorSetUpdate.hpp
@@ -5,7 +5,7 @@
 
 #include "Renderer.hpp"
 #include "DescriptorSetWriteModel.hpp"
-// #include "DescriptorSetCopyModel.hpp"
+#include "DescriptorSetCopyModel.hpp"
 
 namespace kF::Graphics
 {
@@ -26,29 +26,29 @@ struct kF::Graphics::DescriptorSetUpdate
         );
     }
 
-    // /** @brief Copy update of a descriptor set */
-    // static void UpdateCopy(Renderer &renderer,
-    //         const DescriptorSetCopyModel * const copyModelBegin, const DescriptorSetCopyModel * const copyModelEnd) noexcept {
-    //     vkUpdateDescriptorSets(
-    //         renderer.logicalDevice(),
-    //         0,
-    //         nullptr,
-    //         static_cast<std::uint32_t>(std::distance(copyModelBegin, copyModelEnd)), copyModelBegin
-    //     );
-    // }
+    /** @brief Copy update of a descriptor set */
+    static void UpdateCopy(Renderer &renderer,
+            const DescriptorSetCopyModel * const copyModelBegin, const DescriptorSetCopyModel * const copyModelEnd) noexcept {
+        vkUpdateDescriptorSets(
+            renderer.logicalDevice(),
+            0,
+            nullptr,
+            static_cast<std::uint32_t>(std::distance(copyModelBegin, copyModelEnd)), copyModelBegin
+        );
+    }
 
-    // /** @brief Write & copy update of a descriptor set */
-    // static void UpdateWriteAndCopy(Renderer &renderer,
-    //         const DescriptorSetWriteModel * const writeModelBegin, const DescriptorSetWriteModel * const writeModelEnd,
-    //         const DescriptorSetCopyModel * const copyModelBegin, const DescriptorSetCopyModel * const copyModelEnd) noexcept {
-    //     vkUpdateDescriptorSets(
-    //         renderer.logicalDevice(),
-    //         static_cast<std::uint32_t>(std::distance(writeModelBegin, writeModelEnd)),
-    //         writeModelBegin,
-    //         static_cast<std::uint32_t>(std::distance(copyModelBegin, copyModelEnd)),
-    //         copyModelBegin
-    //     );
-    // }
+    /** @brief Write & copy update of a descriptor set */
+    static void UpdateWriteAndCopy(Renderer &renderer,
+            const DescriptorSetWriteModel * const writeModelBegin, const DescriptorSetWriteModel * const writeModelEnd,
+            const DescriptorSetCopyModel * const copyModelBegin, const DescriptorSetCopyModel * const copyModelEnd) noexcept {
+        vkUpdateDescriptorSets(
+            renderer.logicalDevice(),
+            static_cast<std::uint32_t>(std::distance(writeModelBegin, writeModelEnd)),
+            writeModelBegin,
+            static_cast<std::uint32_t>(std::distance(copyModelBegin, copyModelEnd)),
+            copyModelBegin
+        );
+    }
 };
 
 #include "DescriptorSetUpdate.ipp"
